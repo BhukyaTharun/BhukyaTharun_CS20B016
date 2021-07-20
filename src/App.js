f78e1a272144328c81811f7dc6a71905ec53a126
BhukyaTharun <bhukyatharun216@gmail.com>
2021-07-20 10:41:30 +0530

    webops

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import Tharun from './components/title/nav.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Tharun />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/title/nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Tharun(){
+    return(
+        <div>
+        <div>BhukyaTharun</div>
+        <div>CS20B016</div>
+        </div>
+    );
+}
+
+export default Tharun;
